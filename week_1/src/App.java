3e8b63e40a3c76460a8dd69ffdec7e839c74cf9f
강범창 <kang@gangbeomchang-ui-MacBookAir.local>
2023-09-14 23:56:02 +0900

    1주차 폴더 재추가

---
+++ week_1/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+
+    public static void hello_app() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        hello_app();
+        int a = 1;
+        boolean b = 10 > a ? true : false;
+        char c1 = '1', c2 = '2', c3 = '3';
+        System.out.println(a + " hello " + c1 + c2 + c3 + b);
+    }
+}
