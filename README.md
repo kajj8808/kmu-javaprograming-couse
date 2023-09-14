c6d65633e790aae44a1949f5b8a6b4afe5376967
강범창 <kang@gangbeomchang-ui-MacBookAir.local>
2023-09-14 23:52:47 +0900

    2주차 실습 업로드

---
+++ README.md
@@ -0,0 +1,5 @@
+#
+
+# Webstruct Repasitory
+
+계명대학교 Webstruct 에 대한 git 저장소 입니다.
---
+++ week_2/1_2/src/sorry.java
@@ -0,0 +1,5 @@
+public class sorry {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Sorry~~ 재미있어서 미안합니다~~");
+    }
+}
